fbde2d9a2ef15e1a4aa515ff348a74934fc76fbe
Prakhar Agrawal <prakhargvp@gmail.com>
2019-08-28 01:42:10 +0530

    Modify the method getMovies in moviesDAO.js to allow the application to display new pages of movies.

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -261,7 +261,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Paging
     // Use the cursor to only return the movies that belong on the current page
-    const displayCursor = cursor.limit(moviesPerPage)
+    const displayCursor = cursor.limit(moviesPerPage).skip(page * moviesPerPage)
 
     try {
       const moviesList = await displayCursor.toArray()

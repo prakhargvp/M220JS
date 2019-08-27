ad0c1f6452281361c1d3302df19177a6b408ec32
Prakhar Agrawal <prakhargvp@gmail.com>
2019-08-28 00:58:47 +0530

    Text and Subfield Search: updating genreSearchQuery

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -13,6 +13,7 @@ export default class MoviesDAO {
       mflix = await conn.db(process.env.MFLIX_NS)
       movies = await conn.db(process.env.MFLIX_NS).collection("movies")
       this.movies = movies // this is only for testing
+
     } catch (e) {
       console.error(
         `Unable to establish a collection handle in moviesDAO: ${e}`,
@@ -62,6 +63,7 @@ export default class MoviesDAO {
       // here is only included to avoid sending 46000 documents down the
       // wire.
       cursor = await movies.find({ countries: {$in: countries} }, { projection: {title: 1} })
+
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return []
@@ -116,7 +118,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Text and Subfield Search
     // Construct a query that will search for the chosen genre.
-    const query = {}
+    const query = { genres: { $in: searchGenre } }
     const project = {}
     const sort = DEFAULT_SORT
 

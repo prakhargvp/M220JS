611f6e3809226a84e22551f2d54855d06be120ae
Prakhar Agrawal <prakhargvp@gmail.com>
2019-09-03 21:13:15 +0530

    The comments should be returned in order from most recent to least recent using the date key.

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -303,6 +303,21 @@ export default class MoviesDAO {
           $match: {
             _id: ObjectId(id)
           }
+        },
+        {
+          '$lookup': {
+            'from': 'comments', 
+            let: { 'id': '$_id' },
+            pipeline: [
+              {
+                $match: { '$expr': {'$eq': [ '$movie_id', '$$id' ]} }
+              },
+              {
+                $sort: { 'date': -1 }
+              }
+            ],
+            'as': 'comments',
+          }
         }
       ]
       return await movies.aggregate(pipeline).next()

06d75e41bd79e2743d841bd87c10fbe8619f4291
Prakhar Agrawal <prakhargvp@gmail.com>
2019-09-07 17:03:25 +0530

    Handle the error that occurs when an invalid ID is passed to getMovieByID()

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -332,7 +332,10 @@ export default class MoviesDAO {
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
       console.error(`Something went wrong in getMovieByID: ${e}`)
+      if(e.toString().match('Argument passed in must be a single String of 12 bytes or a string of 24 hex characters'))
+        return null
       throw e
+      
     }
   }
 }
